f33e50daa354445b5a9b77bd881c1e977c817a41
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-11-06 22:01:54 -0400

    feat: Atulização e inclusão de funções para ação de cadastro, atualização e exclusão de dados do bd

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 import { db } from "../db.js";
 
-export const getUsers = (_, res) => {
+export const coletarCadastros = (_, res) => {
     const q = "SELECT * FROM users";
 
     db.query(q, (err, data) => {
@@ -9,3 +9,47 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data);
     });
 };
+
+export const criarNovoCadastro = (req, res) => {
+    const q = "INSERT INTO users(`nome`,`email`,`fone`,`valor`) VALUES(?)";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.tag,
+    ];
+
+    db.query(q, [values], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Cadastro criado com sucesso!");
+    })
+}
+
+export const atualizarCadastro = (req, res) => {
+    const q = "UPDATE `heroku_e02dbb19d377403`.`users` SET `nome`=?, `email`=?, `fone`=?, `valor`=? WHERE (`id`= ?) ";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.tag,
+    ];
+
+    db.query(q, [...values, req.params.id], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Cadastro Atualizado!");
+    })
+}
+
+export const deletarCadastro = (req, res) => {
+    const q = "DELETE FROM `heroku_e02dbb19d377403`.`users` WHERE (`id`=?)";
+
+    db.query(q, [req.params.id], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Cadastro deletado com sucesso!");
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 import express from "express";
 
-import { getUsers } from "../controller/user.js";
+import { coletarCadastros, criarNovoCadastro, atualizarCadastro, deletarCadastro } from "../controller/user.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
+router.get("/", coletarCadastros);
+router.post("/", criarNovoCadastro);
+router.put("/:id", atualizarCadastro);
+router.delete("/:id", deletarCadastro);
+
 
 export default router;
