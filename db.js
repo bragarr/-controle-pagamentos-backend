225e91e3d7c05b19233f97c81a4750334f33bff6
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-12-05 10:51:16 -0400

    fix: Reestruturado código para debug de lost connection

--- db.js
+++ db.js
@@ -10,13 +10,6 @@ export const db = mysql.createConnection({
     database: process.env.DB_DATABASE
 })
 
-const db_config = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-}
-
 let conectionActive;
 
 function handleDisconection () {
