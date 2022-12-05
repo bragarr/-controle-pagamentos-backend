36deaaa87fc439b133b67925572222afcc70d1de
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-12-05 10:07:49 -0400

    fix: Aplicando function handle disconnection para o banco de dados

--- db.js
+++ db.js
@@ -9,3 +9,27 @@ export const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 })
+
+let conectionActive;
+
+function handleDisconection () {
+    conectionActive = mysql.createConnection(db);
+
+    conectionActive.connect(function(err) {
+        if(err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconection, 2000);
+        }
+    });
+
+    conectionActive.on('error', function(err) {
+        console.log('db errot', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconection();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconection();
