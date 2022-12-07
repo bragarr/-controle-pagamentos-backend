fdad8e7670a7505941f0b4b8c5732a64d470fc1b
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-12-07 00:24:55 -0400

    fix: Teste e função para conexão com banco

--- db.js
+++ db.js
@@ -10,30 +10,30 @@ const db = mysql.createConnection({
     database: process.env.DB_DATABASE
 });
 
-// let connection;
+let connection;
 
-// function handleDisconnect() {
-//     connection = db;
+function handleDisconnect() {
+    connection = db;
     
-//     connection.connect(function(err) {
-//         if(err) {
-//             console.log("error when connecting to db->", err);
-//             setTimeout(handleDisconnect, 2000);
-//         } else {
-//             console.log("conection is successfull");
-//         }
-//     });
+    connection.connect(function(err) {
+        if(err) {
+            console.log("error when connecting to db->", err);
+            setTimeout(handleDisconnect, 2000);
+        } else {
+            console.log("conection is successfull");
+        }
+    });
 
-//     connection.on("error", function(err) {
-//         console.log("db error", err);
-//         if(err.code==="PROTOCOL_CONNECTION_LOST") {
-//             handleDisconnect();
-//         } else {
-//             throw err;
-//         };
-//     });
-// }
+    connection.on("error", function(err) {
+        console.log("db error", err);
+        if(err.code==="PROTOCOL_CONNECTION_LOST") {
+            handleDisconnect();
+        } else {
+            throw err;
+        };
+    });
+}
 
-// handleDisconnect();
+handleDisconnect();
 
 export default db;
