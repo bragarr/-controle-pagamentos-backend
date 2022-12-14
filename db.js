3cfce98995ebb807c905f3be4e571121a0f3a817
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-12-14 12:23:02 -0400

    fix: Correção de commit para buil de nova API no servidor da Render

--- db.js
+++ db.js
@@ -20,7 +20,7 @@ function handleDisconnect() {
             console.log("error when connecting to db->", err);
             setTimeout(handleDisconnect, 2000);
         } else {
-            console.log("conection is successfull");
+            console.log("conection is successfull!");
         }
     });
 
