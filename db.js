d2e25b8fa33f3f60f8541628faf64901d9d7ea27
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-12-15 15:00:43 -0400

    test: Comentado código de conexão de bd para teste com servidor gratuito

--- db.js
+++ db.js
@@ -9,29 +9,3 @@ export const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
-
-let connection;
-
-function handleDisconnect() {
-    connection = db;
-    
-    connection.connect(function(err) {
-        if(err) {
-            console.log("error when connecting to db->", err);
-            setTimeout(handleDisconnect, 2000);
-        } else {
-            console.log("conection is successfull!");
-        }
-    });
-
-    connection.on("error", function(err) {
-        console.log("db error", err);
-        if(err.code==="PROTOCOL_CONNECTION_LOST") {
-            handleDisconnect();
-        } else {
-            throw err;
-        };
-    });
-}
-
-handleDisconnect();
